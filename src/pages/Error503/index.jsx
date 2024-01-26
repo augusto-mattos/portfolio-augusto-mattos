e7bdded0335f9312504c13b20708641fd92acd16
augusto-mattos <guto.s.mattos@gmail.com>
2024-01-26 14:54:14 +0100

    add Sass and Error503 page

---
+++ src/pages/Error503/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
+function Error503 () {
+    return(
+        <div className="error-container">
+            <div className="error-message">
+            <p>Ce site est en construction.<br /><span>Vous pouvez quand même profiter d'un peu de musique en attendant :) </span></p>
+            </div>
+            <iframe src="https://open.spotify.com/embed/playlist/47KXgL3u6tTXp0t98lCAKw?utm_source=generator&theme=0" width="50%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+    )
+};
+
+export default Error503;
