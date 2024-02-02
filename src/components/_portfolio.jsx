5021a3a6ebfe50410acf35d8d3f1bd0c72f9b43f
augusto-mattos <guto.s.mattos@gmail.com>
2024-02-02 11:54:18 +0100

    add : carrousel

--- src/components/_portfolio.jsx
+++ src/components/_portfolio.jsx
@@ -1,11 +1,39 @@
+import { useState, useEffect } from "react";
+import ProjectsCarousel from "./_projectsCarousel";
 import ProjectsList from "./_projectsList";
 
 function Portfolio() {
+  const [displayAllProjects, setDisplayAllProjects] = useState(false);
+
+  // muda o state para exibir a lista de projetos ou o carrossel. É chamado no botao acima da galeria
+  const toggleDisplayProjects = () => {
+    setDisplayAllProjects(!displayAllProjects);
+  };
+
+  // altera o texto do botao que alterna o modo de visualizacao 
+  const textButton = displayAllProjects ? "Voir carrousel" : "Voir tous";
+
+  // considera o tamanho da tela para exibir a galeria sem a opcao do carrossel
+  useEffect(() => {
+    const handleResize = () => {
+      setDisplayAllProjects(window.innerWidth <= 1024);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div id="portfolio">
       <h3>Portfolio</h3>
       <h4>Consultez mon portfolio</h4>
-      <ProjectsList />
+      <button onClick={toggleDisplayProjects} className="btn-plus">
+        {textButton}
+      </button>
+      {displayAllProjects ? <ProjectsList /> : <ProjectsCarousel />}
     </div>
   );
 }
--- src/components/_projectCard.jsx
+++ src/components/_projectCard.jsx
@@ -1,29 +1,26 @@
 import React from "react";
-import projects from "../data/projects";
 import { Link } from "react-router-dom";
 
-//<Link to={`/logement/${project.id}`}></Link>
+function ProjectCard(props) {
+  const { id, cover, title, resume } = props.project;
 
-function ProjectCard() {
   return (
     <>
-      {projects.map((project) => (
-        <div
-          key={project.id}
-          id={project.id}
-          className="project-card"
-        >
-          <Link to={`/*`}>
-            <img
-              src={process.env.PUBLIC_URL + "/images/" + project.cover}
-              alt="miniature projet"
-              className="project-cover"
-            />
-            <h5>{project.title}</h5>
-            <p>{project.resume}</p>
-          </Link>
-        </div>
-      ))}
+      <div
+        key={id}
+        id={id}
+        className="project-card"
+      >
+        <Link to={`/projet/${id}`}>
+          <img
+            src={process.env.PUBLIC_URL + "/images/" + cover}
+            alt="miniature projet"
+            className="project-cover"
+          />
+          <h5>{title}</h5>
+          <p>{resume}</p>
+        </Link>
+      </div>
     </>
   );
 }
