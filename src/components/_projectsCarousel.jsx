ce79d9c25b4ddccbf958497737360be21058d376
augusto-mattos <guto.s.mattos@gmail.com>
2024-02-05 18:07:09 +0100

    add : slideInterval

--- src/components/_projectsCarousel.jsx
+++ src/components/_projectsCarousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import projects from "../data/projects";
 import ProjectCard from "./_projectCard";
 import arrowLeft from "../assets/icons/arrow-left.png";
@@ -7,6 +7,7 @@ import arrowRight from "../assets/icons/arrow-right.png";
 function ProjectsCarousel() {
     
   const cardsPerPage = 3;
+  const slideInterval = 5000;
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -21,6 +22,16 @@ function ProjectsCarousel() {
         : (prevIndex - 1) % projects.length
     );
   };
+
+  useEffect(() => {
+    const slideTimer = setInterval(() => {
+      nextCard();
+    }, slideInterval);
+
+    return () => {
+      clearInterval(slideTimer);
+    };
+  }, [currentIndex]);
 
   const startIndex = currentIndex % projects.length;
   const endIndex = (startIndex + cardsPerPage) % projects.length;
