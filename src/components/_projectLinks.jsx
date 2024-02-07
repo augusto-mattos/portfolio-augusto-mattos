8c10a8458ea6c5d62d97f13d05d09160af976f1e
augusto-mattos <guto.s.mattos@gmail.com>
2024-02-07 14:54:22 +0100

    change : project links to buttons

--- src/components/_projectLinks.jsx
+++ src/components/_projectLinks.jsx
@@ -1,32 +1,36 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import linkIcon from "../assets/icons/fi-rr-link.png";
 import githubLogo from "../assets/logos/github-mark.png";
 
 function ProjectLinks(props) {
   return (
     <div className="project-links">
-      <NavLink
+      <div className="project-buttons">
+      <Link
         to={props.site}
         target="_blank"
         rel="noreferrer"
+        className="btn-primary"
       >
         <img
           src={linkIcon}
           alt="lien vers le site déployé"
         />
-        {props.site}
-      </NavLink>
-      <NavLink
+        Découvrir le site
+      </Link>
+      <Link
         to={props.repo}
         target="_blank"
         rel="noreferrer"
+        className="btn-primary"
       >
         <img
           src={githubLogo}
           alt="lien vers le repository"
         />
-        {props.repo}
-      </NavLink>
+        Découvrir le code
+      </Link>
+      </div>
       <p>
         <strong>Stack : </strong>
         {props.stack}
