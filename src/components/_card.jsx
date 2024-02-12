a3b4f14e49a9709f01b09619d5bc8b6c0962f2fc
augusto-mattos <guto.s.mattos@gmail.com>
2024-02-12 14:45:38 +0100

    change projectList

---
+++ src/components/_card.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function Card(props) {
+  const { id, cover, title } = props.project;
+  return (
+    <Link
+      to={`/projet/${id}`}
+      key={id}
+    >
+      <div
+        id={id}
+        className="project-card"
+      >
+        <span>{title}</span>
+        <div className="card-overlay" />
+        <img
+          src={process.env.PUBLIC_URL + "/images/" + cover}
+          alt={`slide du projet ${title}`}
+        />
+      </div>
+    </Link>
+  );
+}
+
+export default Card;
