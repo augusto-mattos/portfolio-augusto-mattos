998838f13114b860b0b57d5dad5e783646673a8a
augusto-mattos <guto.s.mattos@gmail.com>
2024-02-12 10:39:39 +0100

    fix : project hero

--- src/components/_projectHero.jsx
+++ src/components/_projectHero.jsx
@@ -1,19 +1,21 @@
 function ProjectHero(props) {
-    return(
-        <div className="project-hero">
-        <h1>{props.title}</h1>
+  return (
+    <div className="project-hero">
+      <h1>{props.title}</h1>
+      <div className="hero-content">
         <div className="project-description">
           <p className="intro">{props.intro}</p>
           <p>{props.description}</p>
+        </div>
         <img
           src={props.img}
           alt="couverture du projet"
           className="hero-img"
           loading="lazy"
         />
-        </div>
       </div>
-    )
+    </div>
+  );
 }
 
 export default ProjectHero;
