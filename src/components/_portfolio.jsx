e5f2283f06c7fe3bbff9f62eb75432c4b63cdf5c
augusto-mattos <guto.s.mattos@gmail.com>
2024-02-13 09:42:33 +0100

    traduction de la section portfolio

--- src/components/_portfolio.jsx
+++ src/components/_portfolio.jsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 import ProjectsList from "./_projectsList";
 import Slider from "./_slider";
 
 function Portfolio() {
+  const { t } = useTranslation();
+
   const [displayAllProjects, setDisplayAllProjects] = useState(false);
 
   const toggleDisplayProjects = () => {
     setDisplayAllProjects(!displayAllProjects);
   };
 
-  const textButton = displayAllProjects ? "Voir carrousel" : "Voir tous";
+  const textButton = displayAllProjects
+    ? t("portfolio-displayCarousel-btn")
+    : t("portfolio-displayAll-btn");
 
   return (
     <section id="portfolio">
       <h3>Portfolio</h3>
-      <h4>Consultez mon portfolio</h4>
-      <button onClick={toggleDisplayProjects} className="btn-plus">
+      <h4>{t("portfolio-subtitle")}</h4>
+      <button
+        onClick={toggleDisplayProjects}
+        className="btn-plus"
+      >
         {textButton}
       </button>
       {displayAllProjects ? <ProjectsList /> : <Slider />}
