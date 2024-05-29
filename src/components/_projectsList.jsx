83a1578e74d3bf19772746b634b1b484ef22b0e3
augusto-mattos <guto.s.mattos@gmail.com>
2024-05-29 11:41:12 +0200

    list of projects : paginacao

--- src/components/_projectsList.jsx
+++ src/components/_projectsList.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import projects from "../locales/fr/fr-projects.json";
 import Card from "./_card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 function ProjectsList() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 6;
+
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(
+    indexOfFirstProject,
+    indexOfLastProject
+  );
+
+  const nextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <>
       <div className="gallery">
-        {projects.map((project) => (
+        {currentProjects.map((project) => (
           <Card
             key={project.id}
             id={project.id}
@@ -14,7 +43,24 @@ function ProjectsList() {
           />
         ))}
       </div>
-    </> 
+      <div className="pagination">
+        <button
+          onClick={prevPage}
+          disabled={currentPage === 1}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} className="chevron" />
+        </button>
+        <span>
+          Page {currentPage} of {totalPages}
+        </span>
+        <button
+          onClick={nextPage}
+          disabled={currentPage === totalPages}
+        >
+          <FontAwesomeIcon icon={faChevronRight} className="chevron"/>
+        </button>
+      </div>
+    </>
   );
 }
 
